b389ae55cb5789e4108a6a12de0562c7adc912b0
“enamul58” <enamullimon@gmail.com>
2023-09-17 23:30:08 +0600

    navbar fixed when scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,36 @@
+function navMenu() {
+  let navBar = document.querySelector(".navbar-sticky");
+  let scrollTopButton = document.querySelector("#scrollUp");
+
+  window.onscroll = function () {
+    var scroll = document.documentElement.scrollTop;
+    console.log(scroll);
+
+    if (scroll >= 120) {
+      navBar.classList.add("navbar-sticky-moved-up");
+    } else {
+      navBar.classList.remove("navbar-sticky-moved-up");
+    }
+
+    // Apply Transition
+    if (scroll >= 250) {
+      navBar.classList.add("navbar-sticky-transitioned");
+      scrollTopButton.classList.add("scrollActive");
+    } else {
+      navBar.classList.remove("navbar-sticky-transitioned");
+      scrollTopButton.classList.remove("scrollActive");
+    }
+
+    //sticky on
+    if (scroll >= 500) {
+      navBar.classList.add("navbar-sticky-on");
+    } else {
+      navBar.classList.remove("navbar-sticky-on");
+    }
+  };
+}
+navMenu();
+
 window.addEventListener("scroll", animate);
 window.addEventListener("load", animate);
 
@@ -6,7 +39,6 @@ function animate() {
 
   for (var i = 0; i < animates.length; i++) {
     var windowHeight = window.innerHeight;
-    console.log(windowHeight);
     var animatetop = animates[i].getBoundingClientRect().top;
 
     var animatePoint = 10;
